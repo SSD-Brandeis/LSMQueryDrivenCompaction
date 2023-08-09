eefe205da02efe414cf84816787fc623274362b3
Shubham <its.shubham.kaushik@gmail.com>
2023-08-09 16:22:53 +0000

    Fixed Last Key Issue

--- development/rocksdb/db/version_set.cc
+++ development/rocksdb/db/version_set.cc
@@ -1630,7 +1630,7 @@ bool LevelIterator::SkipEmptyFileForward() {
       {
         flevel_->files[file_index_].file_metadata->being_compacted = true;
         db_impl_->edits_->DeleteFile(level_, flevel_->files[file_index_].file_metadata->fd.GetNumber());
-        db_impl_->WriteLevelNTable(flevel_, file_index_, level_);
+        db_impl_->FlushLevelNPartialFile(flevel_, file_index_, level_);
       } else if (db_impl_!=nullptr && db_impl_->range_end_key_ != "" && 
                  icomparator_.user_comparator()->Compare(flevel_->files[file_index_].largest_key, Slice(db_impl_->range_end_key_)) <= 0 &&
                  icomparator_.user_comparator()->Compare(flevel_->files[file_index_].smallest_key, Slice(db_impl_->range_start_key_)) >= 0) 

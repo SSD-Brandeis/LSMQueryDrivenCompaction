0803d9c6c7e2e979062a23ea53991ddf11411205
Shubham Kaushik <kaushiks@brandeis.edu>
2024-03-25 14:20:30 +0000

    Updates Script

--- development/rocksdb/examples/__working_branch/run_experiments.py
+++ development/rocksdb/examples/__working_branch/run_experiments.py
@@ -1,19 +1,19 @@
 import os
 import shutil
-import subprocess
 from pathlib import Path
 
 CURR_DIR = Path(__file__).parent
 project_dir = Path(__file__).parent.absolute().__str__()
 # WORKLOAD_DIR = CURR_DIR.joinpath("workloads")
 EXPERIMENT_DIR = (
-    Path(__file__).parent.joinpath("experiments-epoch").absolute().__str__()
+    Path(__file__).parent.joinpath("experiments-E256-B16-Y0.05").absolute().__str__()
 )
 OUTPUT_FILE = Path(__file__).parent.joinpath("stats.txt").absolute().__str__()
 LOG_FILE = Path(__file__).parent.joinpath("logs.txt").absolute().__str__()
 
 if not Path(EXPERIMENT_DIR).exists():
     Path(EXPERIMENT_DIR).mkdir()
+
 
 def create_workload(args, dir_name, log):
     os.chdir(EXPERIMENT_DIR)
@@ -23,11 +23,10 @@ def create_workload(args, dir_name, log):
     workload_file = os.path.join("workload.txt")
 
     if os.path.exists(workload_file):
+        print("Found already generated workload, Replacing ...")
         shutil.copy(workload_file, os.path.join(EXPERIMENT_DIR, dir_name))
     else:
-        process_output = os.popen(
-            f"../../../../emulator/emu_runner {args}"
-        ).read()
+        process_output = os.popen(f"../../../../emulator/emu_runner {args}").read()
         log.write(f"{process_output}\n")
         shutil.copy(workload_file, os.path.join(EXPERIMENT_DIR, dir_name))
     os.chdir(project_dir)
@@ -46,10 +45,9 @@ def run_workload(params, dir_name, log):
         shutil.rmtree(db_dir)
 
     args = f" {params}"
-    log.write(f"../../track_io.sh {args}")
-    process_output = os.popen(
-        f"../../track_io.sh {args}"
-    ).read()
+    log.write(f"../../working_version {args} > workload.log")
+    print(f"Running ../../working_version {args}")
+    process_output = os.popen(f"../../working_version {args} > workload.log").read()
     log.write(f"{process_output}\n")
 
     if os.path.exists(db_dir):
@@ -59,20 +57,24 @@ def run_workload(params, dir_name, log):
     log.write(" -------------- DONE -------------- \n\n\n\n")
     os.chdir(CURR_DIR)
 
+
 if __name__ == "__main__":
     # size_ratio = [2, 4, 6]
     size_ratio = [6]
-    inserts = 31250                           # 1M
-    updates = 7812                            # 250K
+    inserts = 1000000
+    updates = 250000
     range_queries = 100
-    selectivity = 0.1                           # 10 percent (Y)
-    entry_size = 1024 # 32                             # 32 bytes (E)
-    number_of_entries_per_page = 4 # 128            # 128 entries per page (B)
-    number_of_pages = 128                       # 128 pages per file (P)
+    selectivity = 0.05
+    entry_size = 256  # (E)
+    number_of_entries_per_page = 16  # (B)
+    number_of_pages = 128  # (P)
+
+    lower_bounds = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1, 2, 3]
+    upper_bounds = [0.05, 0.1, 0.25, 0.5, 0.75, 1, 2, 4, 6]
 
     buffer_size = entry_size * number_of_entries_per_page * number_of_pages
     log = open(LOG_FILE, "a")
-    
+
     print("Buffer size: ", buffer_size)
 
     os.chdir(EXPERIMENT_DIR)
@@ -80,17 +82,48 @@ if __name__ == "__main__":
     print("Current Dir: ", os.getcwd())
 
     for size in size_ratio:
-        directory_name = f"I {inserts} U {updates} S {range_queries} Y {selectivity} T {size}"
+
+        lower_upper_bounds = list()
+
+        for lb in lower_bounds:
+            for ub in upper_bounds:
+                if lb == 0.01 and ub < 4:
+                    continue
+                if lb < ub:
+                    lower_upper_bounds.append((lb, ub))
+
+        directory_name = (
+            f"I {inserts} U {updates} S {range_queries} Y {selectivity} T {size}"
+        )
         arguments = f"-I {inserts} -U {updates} -S {range_queries} -Y {selectivity} -E {entry_size} -B {number_of_entries_per_page} -P {number_of_pages} -T {size}"
 
-        create_workload(arguments, directory_name + f" rq 0", log)
+        # create_workload(
+        #     arguments,
+        #     directory_name + f" rq 0 E {entry_size} B {number_of_entries_per_page}",
+        #     log,
+        # )
 
-        # vaniall run
-        run_workload(arguments + " --rq 0", directory_name + f" rq 0", log)
+        # # vaniall run
+        # run_workload(
+        #     arguments + " --rq 0",
+        #     directory_name + f" rq 0 E {entry_size} B {number_of_entries_per_page}",
+        #     log,
+        # )
 
-        create_workload(arguments, directory_name + f" rq 1", log)
+        for lower_bound, upper_bound in lower_upper_bounds:
+            create_workload(
+                arguments,
+                directory_name
+                + f" rq 1 E {entry_size} B {number_of_entries_per_page} lb {lower_bound} ub {upper_bound}",
+                log,
+            )
 
-        # rqdc run
-        run_workload(arguments + f" --rq 1 --lb {0.12} --ub {1}", directory_name + " rq 1", log)
+            # rqdc run
+            run_workload(
+                arguments + f" --rq 1 --lb {lower_bound} --ub {upper_bound}",
+                directory_name
+                + f" rq 1 E {entry_size} B {number_of_entries_per_page} lb {lower_bound} ub {upper_bound}",
+                log,
+            )
 
     log.close()

7fb749d473ffdb63c430123fb0f3c3d9f53294f8
Shubham Kaushik <kaushiks@brandeis.edu>
2025-02-16 20:58:56 +0000

    minors

--- runexp.sh
+++ runexp.sh
@@ -21,7 +21,7 @@ SNAP=0
 echo "Starting experiments with TAG=${TAG}, ENTRY_SIZE=${ENTRY_SIZE}"
 echo "Debug: INSERTS=${INSERTS}, UPDATES=${UPDATES}, RANGE_QUERIES=${RANGE_QUERIES}"
 
-LOWER_BOUND=0.1
+LOWER_BOUND=$(echo "scale=9; 1/(${SIZE_RATIO})" | bc)
 
 EXP_DIR="experiments-${TAG}-U${UPDATES}-E${ENTRY_SIZE}-B${ENTRIES_PER_PAGE}-S${RANGE_QUERIES}-Y${SELECTIVITY}-T${SIZE_RATIO}"
 echo "Debug: EXP_DIR=${EXP_DIR}"
@@ -30,7 +30,7 @@ cd .vstats
 mkdir -p $EXP_DIR
 cd $EXP_DIR
 
-mkdir -p VanillaRandom RangeReduceRandom RangeReduceRandomRE1
+mkdir -p VanillaRandom RangeReduceRandom RangeReduceRandomRE1 RangeReduceRandom0
 
 echo "Generating workload..."
 cd VanillaRandom
@@ -57,6 +57,16 @@ else
     exit 1
 fi
 
+echo "Copying workload to RangeReduceRandom0..."
+cd ../RangeReduceRandom0
+if [ -f "../VanillaRandom/workload.txt" ]; then
+    cp ../VanillaRandom/workload.txt ./workload.txt
+    echo "workload.txt copied successfully"
+else
+    echo "Error: workload.txt not found in VanillaRandom"
+    exit 1
+fi
+
 echo "Running VanillaRandom workload..."
 cd ../VanillaRandom
 ../../../bin/working_version -I ${INSERTS} -U ${UPDATES} -S ${RANGE_QUERIES} -Y ${SELECTIVITY} -E ${ENTRY_SIZE} -B ${ENTRIES_PER_PAGE} -P ${PAGES_PER_FILE} -T ${SIZE_RATIO} --rq 0 --progress ${SHOW_PROGRESS} -V ${VERSION} --sanity ${SANITY_CHECK} --usedb ${USE_DB} --snap ${SNAP}
@@ -70,6 +80,11 @@ rm -rf db
 echo "Running RangeReduceRandomRE1 workload [with lb=${LOWER_BOUND} && re=1]..."
 cd ../RangeReduceRandomRE1
 ../../../bin/working_version -I ${INSERTS} -U ${UPDATES} -S ${RANGE_QUERIES} -Y ${SELECTIVITY} -E ${ENTRY_SIZE} -B ${ENTRIES_PER_PAGE} -P ${PAGES_PER_FILE} -T ${SIZE_RATIO} --rq 1 --lb ${LOWER_BOUND} --re 1 --progress ${SHOW_PROGRESS} -V ${VERSION} --sanity ${SANITY_CHECK} --usedb ${USE_DB} --snap ${SNAP}
+rm -rf db
+
+echo "Running RangeReduceRandom workload [with lb=0]..."
+cd ../RangeReduceRandom0
+../../../bin/working_version -I ${INSERTS} -U ${UPDATES} -S ${RANGE_QUERIES} -Y ${SELECTIVITY} -E ${ENTRY_SIZE} -B ${ENTRIES_PER_PAGE} -P ${PAGES_PER_FILE} -T ${SIZE_RATIO} --rq 1 --lb 0 --re 0 --progress ${SHOW_PROGRESS} -V ${VERSION} --sanity ${SANITY_CHECK} --usedb ${USE_DB} --snap ${SNAP}
 rm -rf db
 
 cd ../../..
